253b6228dc2f8b736894f7c5a816f2af3631fb5e
Juan Prieto <juaprifer@alum.us.es>
2026-02-13 18:05:54 +0100

    feat: :sparkles: Spokesperson entity

---
+++ src/main/java/acme/entities/spokesperson/Spokesperson.java
@@ -0,0 +1,38 @@
+package acme.entities.spokesperson;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+import acme.client.components.basis.AbstractRole;
+import acme.client.components.validation.Mandatory;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Spokesperson extends AbstractRole {
+
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Mandatory
+	@Column
+	private String				cv;
+
+	@Mandatory
+	@Column
+	private String				achievements;
+
+	@Mandatory
+	@Column
+	private boolean				licensed;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+}
