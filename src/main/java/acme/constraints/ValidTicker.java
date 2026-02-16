0176415a608605d6247470507b28115dd9403cce
Manuel Zoilo <Manuel Zoilo@PCMANU>
2026-02-16 08:14:40 +0100

    feature: Valid HEader and Valid Ticker

--- src/main/java/acme/constraints/ValidTicker.java
+++ src/main/java/acme/constraints/ValidTicker.java
@@ -20,6 +20,7 @@ import java.lang.annotation.Target;
 import javax.validation.Constraint;
 import javax.validation.Payload;
 import javax.validation.ReportAsSingleViolation;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 @Target(ElementType.FIELD)
@@ -27,8 +28,8 @@ import javax.validation.constraints.Pattern;
 @Constraint(validatedBy = {})
 @ReportAsSingleViolation
 
-@Pattern(regexp = "^[\\w\\-]{5,15}$")
-
+@NotBlank
+@Pattern(regexp = "^[A-Z]{2}[0-9]{2}-\\w{5,10}$")
 public @interface ValidTicker {
 
 	// Standard validation properties -----------------------------------------
