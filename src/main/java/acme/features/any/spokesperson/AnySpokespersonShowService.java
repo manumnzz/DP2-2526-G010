09f4954a3cda122b86ead199b3ed444b1b7178a9
Juan Prieto <juaprifer@alum.us.es>
2026-03-01 12:09:07 +0100

    feat: :sparkles: any spokesperson service, controller, repository

---
+++ src/main/java/acme/features/any/spokesperson/AnySpokespersonShowService.java
@@ -0,0 +1,38 @@
+
+package acme.features.any.spokesperson;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.client.components.principals.Any;
+import acme.client.services.AbstractService;
+import acme.realms.Spokesperson;
+
+@Service
+public class AnySpokespersonShowService extends AbstractService<Any, Spokesperson> {
+
+	@Autowired
+	private AnySpokespersonRepository	repository;
+
+	private Spokesperson				spokesperson;
+
+
+	@Override
+	public void load() {
+		int id;
+
+		id = super.getRequest().getData("id", int.class);
+		this.spokesperson = this.repository.findSpokespersonById(id);
+	}
+
+	@Override
+	public void authorise() {
+		super.setAuthorised(this.spokesperson != null);
+	}
+
+	@Override
+	public void unbind() {
+		super.unbindObject(this.spokesperson, "cv", "achievements", "licensed");
+	}
+
+}
