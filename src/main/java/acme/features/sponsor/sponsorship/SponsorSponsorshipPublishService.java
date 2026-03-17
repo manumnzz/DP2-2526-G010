68566294ea0e520afc5c2d090ee08f35cb0e2d0d
Manuel <manortper1@alum.us,es>
2026-03-17 17:58:37 +0100

    fix: fixed csv and publish

--- src/main/java/acme/features/sponsor/sponsorship/SponsorSponsorshipPublishService.java
+++ src/main/java/acme/features/sponsor/sponsorship/SponsorSponsorshipPublishService.java
@@ -8,9 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import acme.client.components.models.Tuple;
+import acme.client.helpers.MomentHelper;
 import acme.client.services.AbstractService;
 import acme.entities.sponsorships.Donation;
 import acme.entities.sponsorships.Sponsorship;
+import acme.entities.sponsorships.SponsorshipRepository;
 import acme.realms.Sponsor;
 
 @Service
@@ -19,6 +21,9 @@ public class SponsorSponsorshipPublishService extends AbstractService<Sponsor, S
 	@Autowired
 	private SponsorSponsorshipRepository	repository;
 
+	@Autowired
+	private SponsorshipRepository			sponsorshipRepository; // para transients si los muestras
+
 	private Sponsorship						sponsorship;
 
 
@@ -26,6 +31,9 @@ public class SponsorSponsorshipPublishService extends AbstractService<Sponsor, S
 	public void load() {
 		int id = super.getRequest().getData("id", int.class);
 		this.sponsorship = this.repository.findOneSponsorshipById(id);
+
+		if (this.sponsorship != null)
+			this.sponsorship.setRepository(this.sponsorshipRepository);
 	}
 
 	@Override
@@ -54,15 +62,13 @@ public class SponsorSponsorshipPublishService extends AbstractService<Sponsor, S
 		int count = this.repository.countDonationsBySponsorshipId(this.sponsorship.getId());
 		super.state(count > 0, "*", "sponsor.sponsorship.error.no-donations");
 
-		if (!super.getResponse().getErrors().hasErrors("startMoment") && this.sponsorship.getStartMoment() != null) {
-			Date now = new Date();
-			super.state(this.sponsorship.getStartMoment().after(now), "startMoment", "sponsor.sponsorship.error.start-future");
-		}
+		Date now = MomentHelper.getCurrentMoment();
 
-		if (!super.getResponse().getErrors().hasErrors("endMoment") && this.sponsorship.getEndMoment() != null) {
-			Date now = new Date();
+		if (!super.getResponse().getErrors().hasErrors("startMoment") && this.sponsorship.getStartMoment() != null)
+			super.state(this.sponsorship.getStartMoment().after(now), "startMoment", "sponsor.sponsorship.error.start-future");
+
+		if (!super.getResponse().getErrors().hasErrors("endMoment") && this.sponsorship.getEndMoment() != null)
 			super.state(this.sponsorship.getEndMoment().after(now), "endMoment", "sponsor.sponsorship.error.end-future");
-		}
 
 		if (this.sponsorship.getStartMoment() != null && this.sponsorship.getEndMoment() != null)
 			super.state(this.sponsorship.getStartMoment().before(this.sponsorship.getEndMoment()), "endMoment", "sponsor.sponsorship.error.start-before-end");
@@ -83,9 +89,16 @@ public class SponsorSponsorshipPublishService extends AbstractService<Sponsor, S
 	@Override
 	public void unbind() {
 		Tuple tuple;
+		boolean readonly;
 
 		tuple = super.unbindObject(this.sponsorship, "ticker", "name", "description", "startMoment", "endMoment", "moreInfo", "draftMode");
+
+		tuple.put("monthsActive", this.sponsorship.getMonthsActive());
+		tuple.put("totalMoney", this.sponsorship.getTotalMoney());
+
 		tuple.put("id", this.sponsorship.getId());
-		tuple.put("readonly", true);
+
+		readonly = !this.sponsorship.isDraftMode();
+		tuple.put("readonly", readonly);
 	}
 }
